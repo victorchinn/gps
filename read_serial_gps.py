45464db398b53308b78021596df7e45616cd35e3
Victor Chinn <victor.chinn@colbyinstruments.com>
2017-03-05 20:18:25 -0800

    updated file

--- read_serial_gps.py
+++ read_serial_gps.py
@@ -5,26 +5,126 @@ import pynmea2
 lat = 0; 
 lon = 0;
 
+
 with serial.Serial('/dev/ttyS0', baudrate=9600, timeout=1) as ser:
     # read 10 lines from the serial output
     while(1):
         try:
-            line = ser.readline().decode('ascii', errors='replace')
-            #print "read :", (line.strip()),":end--"   
-            print ".",
+            serial_data_line = ser.readline().decode('ascii', errors='replace')
+            GPS_Line = serial_data_line.strip(' ')
+            print "Read :", GPS_Line   
+
         except Exception as e:
             #print "serial port exception"
             #ignored
             pass
 
         try:
-            #decoded = pynmea2.parse('$GPRMC,223114.000,A,4733.8824,N,12206.4382,W,0.06,275.93,050317,,,A*78')
-            decoded = pynmea2.parse(line.strip())
-            lat = (decoded.latitude + lat) / 2.0
-            lon = (decoded.longitude + lon) / 2.0
-            alt = decoded.alt
-            print "\n\rLAT LON ALT:", lat,lon,alt
+            #decoded = pynmea2.parse(' $GPRMC,223114.000,A,4733.8824,N,12206.4382,W,0.06,275.93,050317,,,A*78')
+            #decoded = pynmea2.parse(' $GPVTG,32.80,T,,M,0.08,N,0.16,K,A*0B')
+            #decoded = pynmea2.parse(' $GPGGA,032222.000,4733.8736,N,12206.4312,W,1,08,0.85,214.3,M,-17.3,M,,*5E')
+            
+            GPS_Prefix = GPS_Line[0:6]
+            if (GPS_Prefix == '$GPGSA'):
+                #class GPGSA(NMEASentence):
+                #    def __init__(self):
+                #        parse_map = (
+                #            ('Mode', 'mode'),
+                #            ('Mode fix type', 'mode_fix_type'),
+                #            ('SV ID01', 'sv_id01'),
+                #            ('SV ID02', 'sv_id02'),
+                #            ('SV ID03', 'sv_id03'),
+                #            ('SV ID04', 'sv_id04'),
+                #            ('SV ID05', 'sv_id05'),
+                #            ('SV ID06', 'sv_id06'),
+                #            ('SV ID07', 'sv_id07'),
+                #            ('SV ID08', 'sv_id08'),
+                #            ('SV ID09', 'sv_id09'),
+                #            ('SV ID10', 'sv_id10'),
+                #            ('SV ID11', 'sv_id11'),
+                #            ('SV ID12', 'sv_id12'),
+                #            ('PDOP (Dilution of precision)', 'pdop'),
+                #            ('HDOP (Horizontal DOP)', 'hdop'),
+                #            ('VDOP (Vertical DOP)', 'vdop'))
+                #            #('Checksum', 'checksum'))
+                #
+                #        super(GPGSA, self).__init__(parse_map)
+                decoded = pynmea2.parse(GPS_Line) 
+           
+
+            elif (GPS_Prefix == '$GPRMC'):
+                #class GPRMC(NMEASentence):
+                #    """ Recommended Minimum Specific GPS/TRANSIT Data
+                #    """
+                #    def __init__(self):
+                #        parse_map = (("Timestamp", "timestamp"),
+                #                     ("Data Validity", "data_validity"),
+                #                     ("Latitude", "lat"),
+                #                     ("Latitude Direction", "lat_dir"),
+                #                     ("Longitude", "lon"),
+                #                     ("Longitude Direction", "lon_dir"),
+                #                     ("Speed Over Ground", "spd_over_grnd"),
+                #                     ("True Course", "true_course"),
+                #                     ("Datestamp", "datestamp"),
+                #                     ("Magnetic Variation", "mag_variation"),
+                #                     ("Magnetic Variation Direction", "mag_var_dir"))
+                #                     #("Checksum", "checksum"))
+                #        super(GPRMC, self).__init__(parse_map)
+                decoded = pynmea2.parse(GPS_Line) 
+            
+                lat = decoded.lat 
+                print "LAT:", lat
+            
+                lon = decoded.lon
+                print "LON:", lon
+
+            elif (GPS_Prefix == '$GPVTG'):
+                #    class GPVTG(NMEASentence):
+                #    """ Track Made Good and Ground Speed
+                #    ("True Track made good", "true_track"),
+                #    ("True Track made good symbol", "true_track_sym"),
+                #    ("Magnetic Track made good", "mag_track"),
+                #    ("Magnetic Track symbol", "mag_track_sym"),
+                #    ("Speed over ground knots", "spd_over_grnd_kts"),
+                #    ("Speed over ground symbol", "spd_over_grnd_kts_sym"),
+                #    ("Speed over ground kmph", "spd_over_grnd_kmph"),
+                #    ("Speed over ground kmph symbol", "spd_over_grnd_kmph_sym"))
+                decoded = pynmea2.parse(GPS_Line) 
+
+                print "SPKM:",decoded.spd_over_grnd_kmph
+
+            elif (GPS_Prefix == '$GPGGA'):
+                #class GPGGA(NMEASentence):
+                #    def __init__(self):
+                #        parse_map = (
+                #            ('Timestamp', 'timestamp'),
+                #            ('Latitude', 'latitude'),
+                #            ('Latitude Direction', 'lat_direction'),
+                #            ('Longitude', 'longitude'),
+                #            ('Longitude Direction', 'lon_direction'),
+                #            ('GPS Quality Indicator', 'gps_qual'),
+                #            ('Number of Satellites in use', 'num_sats'),
+                #            ('Horizontal Dilution of Precision', 'horizontal_dil'),
+                #            ('Antenna Alt above sea level (mean)', 'antenna_altitude'),
+                #            ('Units of altitude (meters)', 'altitude_units'),
+                #            ('Geoidal Separation', 'geo_sep'),
+                #            ('Units of Geoidal Separation (meters)', 'geo_sep_units'),
+                #            ('Age of Differential GPS Data (secs)', 'age_gps_data'),
+                #            ('Differential Reference Station ID', 'ref_station_id'))
+                #            #('Checksum', 'checksum'))
+                #
+                #        super(GPGGA, self).__init__(parse_map)
+                decoded = pynmea2.parse(GPS_Line)
+                print "ALT:",decoded.altitude
+
+            elif (GPS_Prefix == '$GPGSV'):
+                #satellites in view
+                decoded = pynmea2.parse(GPS_Line)
+
+                
+            
+           
         except Exception as e:
-            #print "Error decoding :",line.strip()
+            print "Error decoding :", serial_data_line
             #so ignore it if cant decode it
             pass
