13978e9c0e24d66f0f371976a16efac6e5ebb29a
Victor Chinn <victor.chinn@colbyinstruments.com>
2017-03-08 20:06:45 -0800

    working with mqtt

---
+++ webapp/mqtt_client_test_with_gps.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+import time
+import socket
+import paho.mqtt.client as paho
+from sense_with_gps import PiSenseHat
+
+#for GPS measurements
+from gpsreadlib import read_serial_gps
+
+# add the GPS MEASUREMENTS of lat,lon,altitude,# of satellites in view to the payload 
+# globals to hold data for GPS 
+lat = 0 
+lon = 0
+alt = 0
+numsats = 0 
+speed = 0 
+
+#myHostname = 'iot8e3c'
+
+myHostname = 'iot82dd'
+
+localBroker = "iot82dd"     # Local MQTT broker
+localPort   = 1883          # Local MQTT port
+localUser   = "pi"          # Local MQTT user
+localPass = "raspberry"     # Local MQTT password
+localTopic = "iot/sensor"   # Local MQTT topic to monitor
+localTimeOut = 120          # Local MQTT session timeout
+
+# The callback for when a CONNACK message is received from the broker.
+def on_connect(client, userdata, flags, rc):
+    print("CONNACK received with code %d." % (rc))
+
+# The callback for when a PUBLISH message is received from the broker.
+def on_message(client, userdata, msg):
+    print (string.split(msg.payload))
+
+# display one row of blue LEDs (just for fun)
+def displayLine(row):
+    blue = (0, 0, 255)
+    for i in range(0,8):
+        x = i % 8
+        y = (i / 8) + row
+        # print("set pixel x:%d y:%d" % (x,y))
+        pi_sense.set_pixel(x,y,blue)
+        time.sleep(0.02)
+
+
+# Create a sense-hat object
+pi_sense = PiSenseHat()
+print('PI SenseHat Object Created')
+
+# Get hostname
+## Get my machine hostname
+# import pdb; pdb.set_trace()
+if socket.gethostname().find('.') >= 0:
+    hostname=socket.gethostname()
+else:
+    hostname=socket.gethostbyaddr(socket.gethostname())[0]
+
+# Setup to Publish Sensor Data
+mqttc = paho.Client()
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+mqttc.connect(localBroker, localPort, localTimeOut)
+
+# initialize message dictionary
+msg = {'topic':localTopic, 'payload':"", 'qos':0, 'retain':False }
+
+pi_sense.clear_display()
+
+# loop
+print('Getting Sensor Data')
+
+for num in range (10):
+    (lat,lon,alt,numsats,speed) = read_serial_gps(15)        # read 5 sentences
+    sensors = pi_sense.getAllSensors()
+
+    sensors['host'] = hostname
+
+    sensors['location']['lat'] = { 'value':lat }
+    sensors['location']['lon'] = { 'value':lon }
+    sensors['location']['alt'] = { 'value':alt, 'unit':'feet'}
+    sensors['location']['sats'] = { 'value':numsats}
+    # end add for GPS
+
+    msg['payload'] = str(sensors)
+    print("msg[]:"+msg['payload'])
+
+    #   publish(topic, payload=None, qos=0, retain=False)
+    mqttc.publish('iot/sensor', msg['payload'], 1)
+    time.sleep(2.0)
+
+pi_sense.clear_display()
+print('End of MQTT Messages')
+quit()
